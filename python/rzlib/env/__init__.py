b8a5df58436a36341c0f0f162126fd1c88b12107
Richard Zhan <rzhan@andrew.cmu.edu>
2023-11-28 17:03:22 -0500

    env

---
+++ python/rzlib/env/__init__.py
@@ -0,0 +1,1 @@
+from simple_rl_player import SimpleRLPlayer
---
+++ python/rzlib/env/simple_rl_player.py
@@ -0,0 +1,80 @@
+import asyncio
+
+import numpy as np
+from gym.spaces import Box, Space
+from gym.utils.env_checker import check_env
+from rl.agents.dqn import DQNAgent
+from rl.memory import SequentialMemory
+from rl.policy import EpsGreedyQPolicy, LinearAnnealedPolicy
+from tabulate import tabulate
+from keras.layers import Dense, Flatten
+from keras.models import Sequential
+from keras.optimizers.legacy import Adam
+
+from poke_env.environment.abstract_battle import AbstractBattle
+from poke_env.environment import Move
+from poke_env.player import (
+    Gen8EnvSinglePlayer,
+    MaxBasePowerPlayer,
+    ObsType,
+    RandomPlayer,
+    SimpleHeuristicsPlayer,
+    background_cross_evaluate,
+    background_evaluate_player,
+    wrap_for_old_gym_api,
+)
+from poke_env.data import GenData
+
+
+class SimpleRLPlayer(Gen8EnvSinglePlayer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._data: GenData = GenData.from_gen(8)
+
+    def calc_reward(self, last_battle, current_battle) -> float:
+        return self.reward_computing_helper(
+            current_battle, fainted_value=2.0, hp_value=1.0, victory_value=30.0
+        )
+
+    def embed_battle(self, battle: AbstractBattle) -> ObsType:
+        # -1 indicates that the move does not have a base power
+        # or is not available
+        moves_base_power = -np.ones(4)
+        moves_dmg_multiplier = np.ones(4)
+        move: Move
+        for i, move in enumerate(battle.available_moves):
+            moves_base_power[i] = (
+                move.base_power / 100
+            )  # Simple rescaling to facilitate learning
+            if move.type:
+                moves_dmg_multiplier[i] = move.type.damage_multiplier(
+                    battle.opponent_active_pokemon.type_1,
+                    battle.opponent_active_pokemon.type_2,
+                    type_chart=self._data.type_chart,
+                )
+
+        # We count how many pokemons have fainted in each team
+        fainted_mon_team = len([mon for mon in battle.team.values() if mon.fainted]) / 6
+        fainted_mon_opponent = (
+            len([mon for mon in battle.opponent_team.values() if mon.fainted]) / 6
+        )
+
+        # Final vector with 10 components
+        final_vector = np.concatenate(
+            [
+                moves_base_power,
+                moves_dmg_multiplier,
+                [fainted_mon_team, fainted_mon_opponent],
+            ]
+        )
+        return np.float32(final_vector)
+
+    def describe_embedding(self) -> Space:
+        low = [-1, -1, -1, -1, 0, 0, 0, 0, 0, 0]
+        high = [3, 3, 3, 3, 4, 4, 4, 4, 1, 1]
+        return Box(
+            np.array(low, dtype=np.float32),
+            np.array(high, dtype=np.float32),
+            dtype=np.float32,
+        )
